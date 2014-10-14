d16027426b4b362dee4ebbece456ae28f56ce860
brian8759 <brian8759@gmail.com>
2014-10-13 22:25:45 -0400

    adding some sorting features via angular.js filter

--- public/javascripts/ng-app.js
+++ public/javascripts/ng-app.js
@@ -5,7 +5,8 @@
 var polls = angular.module('polls', ['ngRoute', 'pollControllers', 'pollServices']);
 
 // define some deep-linking routes, in the ng-controllers.js, we need to define those 
-// controllers
+// controllers, actually, these three routers are corresponding to three different states
+// once we enter one state, we use corresponding view and controller
 polls.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
 	when('/polls', {
